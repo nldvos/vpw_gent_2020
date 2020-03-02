73791949c3f885086c39ed08670fd8757fadeb6e
Nicolas Devos <nldvos@gmail.com>
2020-03-02 23:07:17 +0100

    add skeleton for jediparcours

---
+++ jediparcours/jediparcours.py
@@ -0,0 +1,17 @@
+
+
+def solve(l):
+    return 0
+
+# input() reads a string with a line of input, stripping the '\n' (newline) at the end.
+# This is all you need for most Kickstart problems.
+t = int(input())  # read a line with a single integer
+for i in range(1, t + 1):
+    l  = [int(s) for s in input().split()]  # read a list of integers
+    arr = []
+    for j in range(l[0]):
+        pass
+
+    print("{} {}".format(i, solve(arr)))
+    # check out .format's specification for more formatting options
+
